c00f4c4814c75900913782a64255780d9a16327f
shahwezjob1 <103096673+shahwezjob1@users.noreply.github.com>
2024-11-23 00:33:28 +0530

    added swagger
    added swagger | removed actuator and reselience 4j | commented cors conf | created controller interface

--- src/main/java/com/example/weatherpredictor/config/WebConfig.java
+++ src/main/java/com/example/weatherpredictor/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+/*
 @Configuration
 public class WebConfig extends WebMvcConfigurationSupport {
     @Override
@@ -11,3 +12,4 @@ public class WebConfig extends WebMvcConfigurationSupport {
         registry.addMapping("/api/**").allowedOrigins("http://localhost:3000");
     }
 }
+*/
