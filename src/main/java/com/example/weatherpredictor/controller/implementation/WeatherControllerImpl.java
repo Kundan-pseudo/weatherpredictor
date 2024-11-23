9175c7ef0644b85f10a42fb7527dd288cfb661dc
kunseal <kundan.seal@publicissapient.com>
2024-11-23 19:43:15 +0530

    Added AWS environment variable

--- src/main/java/com/example/weatherpredictor/controller/implementation/WeatherControllerImpl.java
+++ src/main/java/com/example/weatherpredictor/controller/implementation/WeatherControllerImpl.java
@@ -5,6 +5,8 @@ import com.example.weatherpredictor.model.WeatherResponse;
 import com.example.weatherpredictor.service.WeatherService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,10 +21,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class WeatherControllerImpl implements WeatherController {
 
     private final WeatherService weatherService;
+    private final Environment environment;
+    @Value("${spring.data.aws.accessKeyId}")
+    private String accessKeyId;
 
     @GetMapping
     public ResponseEntity<WeatherResponse> getWeatherForecast(String city) {
         log.debug("WeatherController::getWeatherForecast");
+        log.info(environment.getProperty("AWS_ACCESS_KEY_ID", accessKeyId));
         WeatherResponse response = weatherService.getWeatherForecast(city);
         return ResponseEntity.ok(response);
     }
