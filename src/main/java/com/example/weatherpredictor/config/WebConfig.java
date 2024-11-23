5830e434f98429f705ed0c798c0c18bccf15cc3d
shahwezjob1 <103096673+shahwezjob1@users.noreply.github.com>
2024-11-23 18:35:21 +0530

    Update WebConfig.java
    swagger cross origin issue resolved

--- src/main/java/com/example/weatherpredictor/config/WebConfig.java
+++ src/main/java/com/example/weatherpredictor/config/WebConfig.java
@@ -2,14 +2,12 @@ package com.example.weatherpredictor.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/*
 @Configuration
-public class WebConfig extends WebMvcConfigurationSupport {
+public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**").allowedOrigins("http://localhost:3000");
     }
 }
-*/
