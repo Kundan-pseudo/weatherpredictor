cf59bf3dc0b4d276bf07a101fe9cabe81f2002c0
shahwezjob1 <103096673+shahwezjob1@users.noreply.github.com>
2024-11-24 18:26:59 +0530

    updated yaml secrets
    updated yaml secrets

--- src/main/java/com/example/weatherpredictor/controller/implementation/WeatherControllerImpl.java
+++ src/main/java/com/example/weatherpredictor/controller/implementation/WeatherControllerImpl.java
@@ -5,8 +5,6 @@ import com.example.weatherpredictor.model.WeatherResponse;
 import com.example.weatherpredictor.service.WeatherService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,14 +19,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class WeatherControllerImpl implements WeatherController {
 
     private final WeatherService weatherService;
-    private final Environment environment;
-    @Value("${spring.data.aws.accessKeyId}")
-    private String accessKeyId;
 
     @GetMapping
     public ResponseEntity<WeatherResponse> getWeatherForecast(String city) {
         log.debug("WeatherController::getWeatherForecast");
-        log.info(environment.getProperty("AWS_ACCESS_KEY_ID", accessKeyId));
         WeatherResponse response = weatherService.getWeatherForecast(city);
         return ResponseEntity.ok(response);
     }
